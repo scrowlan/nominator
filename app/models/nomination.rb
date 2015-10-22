42048296335d401cbf75365dd2c5c7618ccab458
Scott Rowland <scrowlan@gmail.com>
2015-10-22 09:11:23 -0500

    update json

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -1,4 +1,12 @@
 class Nomination < ActiveRecord::Base
   belongs_to :employee, dependent: :delete
 
+  def as_json(options={})
+    super(:only => [:reason,:nominator],
+      :include => {
+        :employee => {:only => [:fname,:lname]}
+        }
+      )
+  end
+
 end
